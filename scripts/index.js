a668509539a4a1f3226ff0e55f22e27b6ea24b46
meislifka <meislifka@gmail.com>
2023-09-12 23:51:48 -0400

    adding button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,9 @@ fetch("./stocks.json")
   })
   .then(data => {
 
-    var compTicker = getTicker(data)
-    var guessTicker = getTicker(data)
+    beginGame(data)
 
-    console.log(compTicker)
-    console.log(data[compTicker].name)
-    console.log(data[compTicker].price)
 
-    console.log(guessTicker)
-    console.log(data[guessTicker].name)
-    console.log(data[guessTicker].price)
   });
 
 function getTicker(data) {
@@ -25,8 +18,104 @@ function getTicker(data) {
   //console.log(randIndex)
   // Select a key from the array of keys using the random index
   const randTicker = keys[randIndex]
-
   return (randTicker)
+}
+
+var firstGame = new Boolean(true); // initial value of false
+function beginGame(data) {
+  if (firstGame) {
+    var compTicker = getTicker(data);
+    var guessTicker = getTicker(data);
+
+    var compName = data[compTicker].name;
+    var compTick = compTicker;
+    var compPrice = data[compTicker].price;
+
+    var guessName = data[guessTicker].name;
+    var guessTick = guessTicker;
+    var guessPrice = data[guessTicker].price;
+
+  }
+
+
+  /*
+    console.log(compTicker);
+    console.log(data[compTicker].name);
+    console.log(data[compTicker].price);
+  
+    console.log(guessTicker)
+    console.log(data[guessTicker].name)
+    console.log(data[guessTicker].price)
+  */
+
+  document.getElementById("compStock-name").innerText = compName;
+  document.getElementById("compStock-ticker").innerText = compTick;
+  document.getElementById("compStock-price").innerText = `$ ${compPrice}`;
+
+  document.getElementById("guessStock-name").innerText = guessName;
+  document.getElementById("guessStock-ticker").innerText = guessTick;
+  console.log(guessPrice);
+
+  document.getElementById('js-higher-button')
+    .addEventListener('click', () => {
+      if (guessPrice > compPrice) {
+        console.log("correct")
+
+        document.getElementById("result").innerText = `Correct: price: $${guessPrice}`;
+        setTimeout(() => {
+          console.log("wait 2 seconds");
+          nextGame(guessName, guessTick, guessPrice);
+        }, 2000);
+      } else {
+        console.log("wrong")
+
+        document.getElementById("result").innerText = `Incorrect: price: $${guessPrice}`;
+        setTimeout(() => {
+          console.log("wait 2 seconds");
+          endGame();
+        }, 2000);
+      }
+
+    });
+
+  document.getElementById('js-lower-button')
+    .addEventListener('click', () => {
+      if (guessPrice < compPrice) {
+        console.log("correct");
+
+        document.getElementById("result").innerText = `Correct: price: $${guessPrice}`;
+        setTimeout(() => {
+          console.log("wait 2 seconds");
+          nextGame(guessName, guessTick, guessPrice);
+        }, 2000);
+      }
+      else {
+        console.log("wrong");
+
+        document.getElementById("result").innerText = `Incorrect: price: $${guessPrice}`;
+        setTimeout(() => {
+          console.log("wait 2 seconds");
+          endGame();
+        }, 2000);
+      }
+
+    });
+}
+
+function nextGame(prevGuessName, prevGuessTick, PrevGuessPrice) {
+  document.getElementById("compStock-name").innerText = prevGuessName;
+  document.getElementById("compStock-ticker").innerText = prevGuessTick;
+  document.getElementById("compStock-price").innerText = `$ ${PrevGuessPrice}`;
+  var guessTicker = getTicker(data);
+
+
 
 }
 
+
+
+
+function endGame() {
+  alert("you lose");
+
+}
