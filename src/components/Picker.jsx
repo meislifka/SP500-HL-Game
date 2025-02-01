f31ab4748b83d1565696ba473a96c23e5ebcab44
Mei Slifka <meislifka@gmail.com>
2025-01-31 23:21:59 -0500

    Changed buttons

--- src/components/Picker.jsx
+++ src/components/Picker.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import IconButton from '@mui/material/IconButton';
-import ArrowUpIcon from '@mui/icons-material/ArrowDropUpSharp';
-import ArrowDownIcon from '@mui/icons-material/ArrowDropDownSharp';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 const Picker = ({ props }) => {
   return (
     <div className="text-center font-arial  p-4 border-2">
+      <Tooltip title="Higher">
        <IconButton >
-        <ArrowUpIcon sx={{ fontSize: 80, color:"green" }} />
+        <ShowChartIcon sx={{ fontSize: 80, color:"green" }} />
       </IconButton>
+      </Tooltip>
+      <div></div>
+      <Tooltip title="Lower">
       <IconButton>
-        <ArrowDownIcon sx={{ fontSize: 80, color:"red" }} />
+        <ShowChartIcon sx={{ fontSize: 80, color:"red",  transform: "scaleX(-1)" }} />
       </IconButton>
+      </Tooltip>
     </div>
   );
 };
